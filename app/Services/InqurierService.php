87fb069a827f2fc5d6654725cd4df026157fe9b2
Ilhaam Akmal <ilhaam43@gmail.com>
2021-08-06 04:09:20 +0700

    fix: bug in inquiry page

--- app/Services/InqurierService.php
+++ app/Services/InqurierService.php
@@ -88,19 +88,13 @@ class InqurierService
 
     public function addInquiryData($request)
     {  
-        $user = Auth::user();    
-
-        $researchJobs = ResearchJobs::where('id', $request['research_jobs_id'])->first();
-
-        return $request;
-
-        $inquiryCount = $researchJobs->count_inquiry;
-
-        if($inquiryCount == 1){
-            return back()->withError('Inquiry data already added in this cycle, please inquiry another company');
-        }  
-        
+        $user = Auth::user();             
         try{   
+            $researchJobs = ResearchJobs::where('id', $request['research_jobs_id'])->first();
+
+            if($researchJobs->count_inquiry == 1){
+                return back()->withError('Inquiry data already added in this cycle, please inquiry another company');
+            }
 
             $request['user_id'] = $user['id'];
             $request['job_status_id'] = 3;
@@ -119,7 +113,6 @@ class InqurierService
             ]);
 
         } catch(\Throwable $th) {
-            return $th;
             return back()->withError('Inquiry data failed to add');
         }
         
